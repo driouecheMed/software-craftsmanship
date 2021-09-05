a45da36890cd0c0885a5dc67d26a150c0edc5e31
Drioueche Mohammed <57846711+driouecheMed@users.noreply.github.com>
2021-09-05 10:44:28 +0100

    sum-two-large-numbers.py

---
+++ sum-two-large-numbers.py
@@ -0,0 +1,38 @@
+'''
+Problem link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Write your code here
+    sum = 0
+    for n in ar:
+        sum = sum + n
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
